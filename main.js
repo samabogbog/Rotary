c516ee71eef009aa8821472fc2910d7c69c6e3cb
Khunrambo <95111793+samabogbog@users.noreply.github.com>
2022-03-14 23:12:29 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -124,7 +124,8 @@ function copyHY() {
     window.getSelection().removeAllRanges();
 }
 
-
+var deg2 = 337.5;
+var scale2 = 1;
             
 function preview2() {
     var preview_box = document.getElementById("preview_box2");
@@ -137,22 +138,64 @@ function preview2() {
 
     preview_box.innerHTML = '<div class="row-box2"></div>';
 
-    //rowBox[0].innerHTML += '<div class="simple-text">LOREM<br>IPSUM</div>'
+    // function Button 2 Ways
+    var expand2 = document.getElementById('expand2'),
+    short2 = document.getElementById('short2'),
+    rotate_c2 = document.getElementById('rotate_c2'),
+    rotate_cc2 = document.getElementById('rotate_cc2');
 
     for(i=0; i<30; i++) {
-        rowBox[0].innerHTML += '<div class="simple-text2">LOREM<br>IPSUM</div>'
-/*
-        simpleText[i].style.left = (HX2 * i) + 'px';
-        simpleText[i].style.top = (HY2 * i) + 'px';
-*/ 
+        rowBox[0].innerHTML += '<div class="simple-text2">LOREM<br>IPSUM</div>' 
+    }
+    expand2.onclick = function() {
+        scale2 += 0.125;
+        if (scale2 > 2) {
+            scale2 = 2;
+        }
+        for(i=0; i<simpleText.length;i++) {
+            simpleText[i].style.transform = 'translate(-50%, -50%) rotate(' + deg2 + 'deg) scale('+ scale2 +')';
+        }
+        return scale2;
+    }
+    short2.onclick = function() {
+        scale2 -= 0.125;
+        if (scale2 < 0.5) {
+            scale2 = 0.5;
+        }
+        for(i=0; i<simpleText.length;i++) {
+            simpleText[i].style.transform = 'translate(-50%, -50%) rotate(' + deg2 + 'deg) scale('+ scale2 +')';
+        }
+        return scale2;
+    }
+    rotate_c2.onclick = function() {
+        deg2 -= 11.25;
+        if (deg2 == 0) {
+            deg2 = 360;
+        }
+        for(i=0; i<simpleText.length;i++) {
+            simpleText[i].style.transform = 'translate(-50%, -50%) rotate(' + deg2 + 'deg) scale('+ scale2 +')';
+        }
+        return deg2;
+    }
+    rotate_cc2.onclick = function() {
+        deg2 += 11.25;
+        if (deg2 == 360) {
+            deg2 = 0;
+        }
+        for(i=0; i<simpleText.length;i++) {
+            simpleText[i].style.transform = 'translate(-50%, -50%) rotate(' + deg2 + 'deg) scale('+ scale2 +')';
+        }
+        return deg2;
     }
     for(x=0; x<15; x++) {
         simpleText[x].style.left = (preview_box_width/2) + (HX2 * x) + 'px';
         simpleText[x].style.top = (preview_box_height/2) + (HY2 * x) + 'px';
+        simpleText[x].style.transform = 'translate(-50%, -50%) rotate(' + deg2 + 'deg) scale('+ scale2 +')';
     }
     for(y=15; y<30; y++) {
         simpleText[y].style.left = (preview_box_width/2) - (HX2 * (y-14)) + 'px';
         simpleText[y].style.top = (preview_box_height/2) - (HY2 * (y-14)) + 'px';
+        simpleText[y].style.transform = 'translate(-50%, -50%) rotate(' + deg2 + 'deg) scale('+ scale2 +')';
     }
     for(i=0; i<29; i++) {
         var clone = rowBox[i].cloneNode(true);
@@ -169,6 +212,8 @@ function preview2() {
 }
 preview2();
 
+var deg4 = 337.5;
+var scale4 = 1;
 function preview4() {
     var preview_box4 = document.getElementById("preview_box4");
     var rowBox4 = document.getElementsByClassName('row-box4');
@@ -182,26 +227,65 @@ function preview4() {
 
     preview_box4.innerHTML = '<div class="row-box4"></div>';
 
-    //rowBox[0].innerHTML += '<div class="simple-text">LOREM<br>IPSUM</div>'
-
+    // function Button 4 Ways
+    var expand4 = document.getElementById('expand4'),
+    short4 = document.getElementById('short4'),
+    rotate_c4 = document.getElementById('rotate_c4'),
+    rotate_cc4 = document.getElementById('rotate_cc4');
+    
     for(i=0; i<30; i++) {
         rowBox4[0].innerHTML += '<div class="simple-text4">LOREM<br>IPSUM</div>'
-/*
-        simpleText[i].style.left = (HX2 * i) + 'px';
-        simpleText[i].style.top = (HY2 * i) + 'px';
-*/ 
     }
-    
+    expand4.onclick = function() {
+        scale4 += 0.125;
+        if (scale4 > 2) {
+            scale4 = 2;
+        }
+        for(i=0; i<simpleText4.length;i++) {
+            simpleText4[i].style.transform = 'translate(-50%, -50%) rotate(' + deg4 + 'deg) scale('+ scale4 +')';
+        }
+        return scale4;
+    }
+    short4.onclick = function() {
+        scale4 -= 0.125;
+        if (scale4 < 0.5) {
+            scale4 = 0.5;
+        }
+        for(i=0; i<simpleText4.length;i++) {
+            simpleText4[i].style.transform = 'translate(-50%, -50%) rotate(' + deg4 + 'deg) scale('+ scale4 +')';
+        }
+        return scale4;
+    }
+    rotate_c4.onclick = function() {
+        deg4 -= 11.25;
+        if (deg4 == 0) {
+            deg4 = 360;
+        }
+        for(i=0; i<simpleText4.length;i++) {
+            simpleText4[i].style.transform = 'translate(-50%, -50%) rotate(' + deg4 + 'deg) scale('+ scale4 +')';
+        }
+        return deg4;
+    }
+    rotate_cc4.onclick = function() {
+        deg4 += 11.25;
+        if (deg4 == 360) {
+            deg4 = 0;
+        }
+        for(i=0; i<simpleText4.length;i++) {
+            simpleText4[i].style.transform = 'translate(-50%, -50%) rotate(' + deg4 + 'deg) scale('+ scale4 +')';
+        }
+        return deg4;
+    }
     for(x=0; x<15; x++) {
         simpleText4[x].style.left = (preview_box_width4/2) + (HX4 * x) + 'px';
-        
         simpleText4[x].style.top = (preview_box_height4/2) + (HY4 * x) + 'px';
+        simpleText4[x].style.transform = 'translate(-50%, -50%) rotate(' + deg4 + 'deg) scale('+ scale4 +')';
     }
     for(y=15; y<30; y++) {
         simpleText4[y].style.left = (preview_box_width4/2) - (HX4 * (y-14)) + 'px';
         simpleText4[y].style.top = (preview_box_height4/2) - (HY4 * (y-14)) + 'px';
+        simpleText4[y].style.transform = 'translate(-50%, -50%) rotate(' + deg4 + 'deg) scale('+ scale4 +')';
     }
-
     for(i=0; i<29; i++) {
         var clone = rowBox4[i].cloneNode(true);
         preview_box4.append(clone);
@@ -214,9 +298,9 @@ function preview4() {
         rowBox4[i].style.top = (preview_box_height4/2) - (vy4_result * (i-14)) + 'px';
         rowBox4[i].style.left = (preview_box_width4/2) - (vx4_result * (i-14)) + 'px';
      }
-
 }
 preview4();
+
 // animate animClass
 var elem = document.getElementsByClassName("notif");
 function anim2() {
